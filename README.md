5cc5e838c3ba54ea47077dd1ae6839defb1b0abb
Mario Torres <mario291187@gmail.com>
2016-06-14 17:02:08 -0500

    main

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # edca_dashboard
+
+## Instalación:
+npm install
+
+Hola cd public/ 
+bower install
+git clone https://github.com/mxabierto/assets.git
+git clone https://github.com/mxabierto/dgm-footer.git
+git clone https://github.com/mxabierto/dgm-navbar.git
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Estandar de Datos de Contrataciones Abiertas' });
+    res.render('index', { title: 'Estandar de Datos de Contrataciones Abiertas' });
 });
+
 
 module.exports = router;
