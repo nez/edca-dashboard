300c4c209785074960659b3ab82d475ca0395c2c
Mario Torres <mario291187@gmail.com>
2016-06-16 13:14:07 -0500

    tabla

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -209,7 +209,7 @@ function searchbykeyword ( keyword, contracts, table ){
     var html = "<table class='table table-striped'>"+
         "<thead>"+
         "<tr>"+
-        "<th>#</th><th style='width: 180px'>Contrato</th><th>Año</th><th>Producto o servicio</th><th>Monto (IVA)</th><th>Descarga</th>"+
+        "<th>#</th><th>Producto o servicio</th><th>Año</th><th style='width: 180px'>Contrato</th><th>Monto (IVA)</th><th>Descarga</th>"+
         "</tr>"+
         "</thead>"+
         "<tbody>";
@@ -220,18 +220,18 @@ function searchbykeyword ( keyword, contracts, table ){
 
             if ( contracts[i].id.toUpperCase().indexOf(keyword.toUpperCase()) != -1 || contracts[i].product.toUpperCase().indexOf(keyword.toUpperCase()) != -1 ) {
                 html += "<tr><td>"+ contracts[i].number+"</td>"+
-                    "<td>"+contracts[i].id+"</td>"+
-                    "<td>"+contracts[i].year+"</td>"+
                     "<td>"+contracts[i].product+"</td>"+
+                    "<td>"+contracts[i].year+"</td>"+
+                    "<td>"+contracts[i].id+"</td>"+
                     "<td>"+contracts[i].amount+"</td>"+
                     "<td> <a href='"+contracts[i].url+"' class='btn btn-default'> <i class='fa fa-download'></i></a></td></tr>";
             }
 
         } else {
             html += "<tr><td>"+ contracts[i].number+"</td>"+
-                "<td>"+contracts[i].id+"</td>"+
-                "<td>"+contracts[i].year+"</td>"+
                 "<td>"+contracts[i].product+"</td>"+
+                "<td>"+contracts[i].year+"</td>"+
+                "<td>"+contracts[i].id+"</td>"+
                 "<td>"+contracts[i].amount+"</td>"+
                 "<td> <a href='"+contracts[i].url+"' class='btn btn-default'> <i class='fa fa-download'></i></a></td></tr>";
         }
