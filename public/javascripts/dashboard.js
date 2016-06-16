f2422dafdbbff9fbf9e0805da63027ed626a5313
Mario Torres <mario291187@gmail.com>
2016-06-16 00:24:33 -0500

    Fonts

---
+++ public/javascripts/dashboard.js
@@ -0,0 +1,255 @@
+
+var contracts = [
+    {
+        number : '1',
+        id: 'LO-009KDH999-N5-2014',
+        year: '2015',
+        product : 'Servicio de extracción de ademes y restitución de terreno.',
+        amount: '$39724276.56',
+        url : ''
+    },
+    {
+        number : '2',
+        id: 'SO-009KDH999-N24-2015',
+        year: '2015',
+        product : 'Plan acciones de monitoreo y conservación de aves para el NAICM.',
+        amount: '$521999.84',
+        url : ''
+    },
+    {
+        number : '3',
+        id: 'SO-009KDH999-N23-2015',
+        year: '2015',
+        product : 'Elaboración del programa de monitoreo de ruido perimetral para las etapas de preparación del sitio, construcción, y, operación y mantenimiento del NAICM.',
+        amount: '$638000.00',
+        url : ''
+    },
+    {
+        number : '4',
+        id: 'SO-009KDH999-N26-2015',
+        year: '2015',
+        product : 'Plan de monitoreo, registro y verificación de emisión de gases de efecto invernadero para el NAICM.',
+        amount: '$539400.00',
+        url : ''
+    },
+    {
+        number : '5',
+        id: 'IO-009KDH999-N11-2015',
+        year: '2015',
+        product : 'Construcción de caminos de acceso a zona del edificio terminal de la primera etapa, necesarios para la caracterización geotécnica inical.',
+        amount: '$9429941.46',
+        url : ''
+    },
+    {
+        number : '6',
+        id: 'IO-009KDH999-N10-2015',
+        year: '2015',
+        product : 'Servicios de supervisión para la obra: extracción de ademes y restitución del terreno que ocupan dichos elementos ubicados en la poligonal para el Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$2501985.67',
+        url : ''
+    },
+    {
+        number : '7',
+        id: 'LO-099KDH999-N20-2015',
+        year: '2015',
+        product : 'Construcción de caminos de accesos y plataformas para exploración geotécnica para la pista No 2 del Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$5844840.96',
+        url : ''
+    },
+    {
+        number : '8',
+        id: 'LO-099KDH999-T15-2015',
+        year: '2015',
+        product : 'Estudio de las caracteristicas técnicas de infraestructura y diseño esquematico de la terminal de carga del Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$13514000.00',
+        url : ''
+    },
+    {
+        number : '9',
+        id: 'LO-009KDH999-N46-2015',
+        year: '2015',
+        product : 'Plan de monitoreo y conservación de aves del proyecto del Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$16684244.272',
+        url : ''
+    },
+    {
+        number : '10',
+        id: 'LO-009KDH999-N42-2015',
+        year: '2015',
+        product : 'Implementación del programa de rescate de flora y fauna para el Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$13832891.6792',
+        url : ''
+    },
+    {
+        number : '11',
+        id: 'LO-009KDH999-N45-2015',
+        year: '2015',
+        product : 'Implementación del plan de restauración ecológica para el Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$21963669.4016',
+        url : ''
+    },
+    {
+        number : '12',
+        id: 'IO-009KDH999-N41-2015',
+        year: '2015',
+        product : 'La elaboración del estudio de las características topográficas del terreno del Nuevo Aeropuerto Internacional de la Ciudad de México (NAICM).',
+        amount: '$38949488.17',
+        url : ''
+    },
+    {
+        number : '13',
+        id: 'IO-009KDH999-N54-2015',
+        year: '2015',
+        product : 'Supervisión para: construcción del proyecto integral consistente en barda y camino perimetral, alumbrado, servicios inducidos y casetas de acceso para el Nuevo Aeropuerto Internacional de la Ciudad de México (primera etapa).',
+        amount: '$3812525.54',
+        url : ''
+    },
+    {
+        number : '14',
+        id: 'LO-009KDH999-N47-2015',
+        year: '2015',
+        product : 'Desarrollo del proyecto ejecutivo, construcción, equipamiento de mobiliario e instalaciones complementarias para el campamento del Grupo Aeroportuario de la Ciudad de México en el sitio del Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$129973731.20',
+        url : ''
+    },
+    {
+        number : '15',
+        id: 'LO-009KDH999-N50-2015',
+        year: '2015',
+        product : 'Trabajos relativos a la construcción del drenaje pluvial temporal del Nuevo Aeropuerto de la Ciudad de México.',
+        amount: '$223329692.62',
+        url : ''
+    },
+    {
+        number : '16',
+        id: 'LO-009KDH999-T52-2015',
+        year: '2015',
+        product : 'Diseñar e instrumentar el modelo virtual de información para la construcción del BIM del NAICM.',
+        amount: '$158629469.03',
+        url : ''
+    },
+    {
+        number : '17',
+        id: 'LO-009KDH999-N79-2015',
+        year: '2015',
+        product : 'Convocatoria de nivelación y limpieza del terreno del Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$2044851759.9544',
+        url : ''
+    },
+    {
+        number : '18',
+        id: 'LO-009KDH999-N80-2015',
+        year: '2015',
+        product : 'Convocatoria de construcción de los caminos provisionales de acceso al Nuevo Aeropuerto Internacional de la Ciudad de México (NAICM) ejecutando trabajos de terraceria, estructuras y obras de drenaje, pavimentación y señalamiento.',
+        amount: '$705599669.90',
+        url : ''
+    },
+    {
+        number : '19',
+        id: 'LO-009KDH999-N78-2015',
+        year: '2015',
+        product : 'Convocatoria de carga, acarreo y descarga fuera del poligono del material existente producto de depositos de demoliciones, desperdicios y desazolve.',
+        amount: '$572943111.00',
+        url : ''
+    },
+    {
+        number : '20',
+        id: 'LO-009KDH999-N86-2015',
+        year: '2015',
+        product : 'Supervisión técnica, administrativa y de control de calidad para los trabajos de nivelación y limpieza del terreno del Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$11276263.6272',
+        url : ''
+    },
+    {
+        number : '21',
+        id: 'LO-009KDH999-N87-2015',
+        year: '2015',
+        product : 'Supervisión técnica, administrativa y de control de calidad para los trabajos de construcción de los caminos provisionales de acceso al Nuevo Aeropuerto Intenacional de la Ciudad de México (NAICM) ejecutanto trabajos de terraceria, estructuras y obras de drenaje, pavimentación y señalamiento.',
+        amount: '$13970275.444',
+        url : ''
+    },
+    {
+        number : '22',
+        id: 'AO-009KDH999-E26-2016',
+        year: '2016',
+        product : 'Supervisión del proyecto ejecutivo, construcción, equipamiento, mobiliario e instalaciones complementarias para el campamento del Grupo Aeroportuario de la Ciudad de México en el sitio del Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$4286458.44',
+        url : ''
+    },
+    {
+        number : '23',
+        id: 'AO-009KDH999-E20-2016',
+        year: '2016',
+        product : 'Supervisión de la construcción del drenaje pluvial temporal del Nuevo Aeropuerto Intenacional de la Ciudad de México.',
+        amount: '$8909803.13',
+        url : ''
+    },
+    {
+        number : '24',
+        id: 'IO-009KDH999-E15-2016',
+        year: '2016',
+        product : 'Construcción de accesos y plataformas para exploración geotécnica, rehabilitación de tramos de prueba y bacheo somero de caminos existentes dentro del terreno para el Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$12715541.31',
+        url : ''
+    },
+    {
+        number : '25',
+        id: 'IO-009KDH999-E96-2015',
+        year: '2016',
+        product : 'Integración de la estratégia global y modelo de negocios para el Nuevo Aeropuerto Internacional de la Ciudad de México.',
+        amount: '$91640000.00',
+        url : ''
+    }
+];
+
+
+
+function searchbykeyword ( keyword, contracts, table ){
+
+    var html = "<table class='table table-striped'>"+
+        "<thead>"+
+        "<tr>"+
+        "<th>#</th><th style='width: 180px'>Contrato</th><th>Año</th><th>Producto o servicio</th><th>Monto (IVA)</th><th>Descarga</th>"+
+        "</tr>"+
+        "</thead>"+
+        "<tbody>";
+
+    for (var i =0; i < contracts.length; i++){
+
+        if ( keyword != '' ){
+
+            if ( contracts[i].id.toUpperCase().indexOf(keyword.toUpperCase()) != -1 || contracts[i].product.toUpperCase().indexOf(keyword.toUpperCase()) != -1 ) {
+                html += "<tr><td>"+ contracts[i].number+"</td>"+
+                    "<td>"+contracts[i].id+"</td>"+
+                    "<td>"+contracts[i].year+"</td>"+
+                    "<td>"+contracts[i].product+"</td>"+
+                    "<td>"+contracts[i].amount+"</td>"+
+                    "<td> <a href='"+contracts[i].url+"' class='btn btn-default'> <i class='fa fa-download'></i></a></td></tr>";
+            }
+
+        } else {
+            html += "<tr><td>"+ contracts[i].number+"</td>"+
+                "<td>"+contracts[i].id+"</td>"+
+                "<td>"+contracts[i].year+"</td>"+
+                "<td>"+contracts[i].product+"</td>"+
+                "<td>"+contracts[i].amount+"</td>"+
+                "<td> <a href='"+contracts[i].url+"' class='btn btn-default'> <i class='fa fa-download'></i></a></td></tr>";
+        }
+
+    }
+
+    html += "</tbody></table>";
+
+    table.html(html);
+
+}
+
+var ctable = $('#ctable');
+
+$('#buscar').keyup(function () {
+    searchbykeyword($(this).val(), contracts, ctable);
+});
+
+$(document).ready(function(){
+    searchbykeyword('', contracts, ctable);
+});
