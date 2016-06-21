5e3cf5c725dbb80451d2a027f7d448cb4d8dbeb1
Mario Torres <mario291187@gmail.com>
2016-06-21 12:11:29 -0500

    db

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,44 +1,6 @@
 function searchbykeyword ( keyword, table ){
-
-    $.post('/contrataciones-abiertas/contracts', function (contracts) {
-
-        var html = "<table class='table table-striped'>"+
-            "<thead>"+
-            "<tr>"+
-            "<th>#</th><th>Producto o servicio</th><th>Año</th><th style='width: 180px'>Contrato</th><th>Monto (IVA)</th><th>Descarga</th><th>Explora</th>"+
-            "</tr>"+
-            "</thead>"+
-            "<tbody>";
-
-        for (var i =0; i < contracts.length; i++){
-
-            if ( keyword != '' ){
-
-                if ( contracts[i].id.toUpperCase().indexOf(keyword.toUpperCase()) != -1 || contracts[i].product.toUpperCase().indexOf(keyword.toUpperCase()) != -1 ) {
-                    html += "<tr><td>"+ contracts[i].number+"</td>"+
-                        "<td>"+contracts[i].product+"</td>"+
-                        "<td>"+contracts[i].year+"</td>"+
-                        "<td>"+contracts[i].id_naicm+"</td>"+
-                        "<td>"+contracts[i].amount+"</td>"+
-                        "<td> <a href='"+contracts[i].download_url+"' class='btn btn-default'> <i class='fa fa-download'></i></a></td>"+
-                        "<td> <a href='"+contracts[i].url+"' class='btn btn-default'> <i class='fa fa-line-chart'></i></a></td></tr>";
-                }
-
-            } else {
-                html += "<tr><td>"+ contracts[i].number+"</td>"+
-                    "<td>"+contracts[i].product+"</td>"+
-                    "<td>"+contracts[i].year+"</td>"+
-                    "<td>"+contracts[i].id_naicm+"</td>"+
-                    "<td>"+contracts[i].amount+" "+contracts[i].currency+"</td>"+
-                    "<td> <a href='"+contracts[i].download_url+"' class='btn btn-default'> <i class='fa fa-download'></i></a></td>"+
-                    "<td> <a href='"+contracts[i].url+"' class='btn btn-default'> <i class='fa fa-line-chart'></i></a></td></tr>";
-            }
-
-        }
-
-        html += "</tbody></table>";
-
-        table.html(html);
+    $.post('/contrataciones-abiertas/find-contracts', { keyword: keyword }, function (contracts) {
+        table.html(contracts);
     });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var pgp = require('pg-promise')();
+var edca_db  = pgp("postgres://tester:test@localhost/edca");
+
+
 var contracts = [
     {
         number : '1',
@@ -345,12 +349,34 @@ router.post('/contracts', function (req, res) {
 
 /* contract details */
 router.get('/contrato/:id',function (req, res) {
-    res.render('tree',{  contract : contracts[ req.params.id -1 ] });
+    edca_db.one('select * from contract where id = $1 ', [ req.params.id ]).then(function (data) {
+        res.render('tree',{  contract : data });
+
+    }).catch(function (error) {
+        console.log("ERROR: ", error);
+        res.render ('tree' );
+    });
+
 });
+
+/* find contract */
+router.post ('/find-contracts/', function (req, res ) {
+    edca_db.manyOrNone("select * from contract where title ilike '%$1#%' or contractid ilike '%$1#%' ", [req.body.keyword]).then(function (data) {
+        res.render (  'contracts', {contracts: data });
+    }).catch(function (error) {
+       console.log("ERROR: ", error); 
+    });
+    
+});
+
 
 /* dashboard contract list (1st page) */
 router.get('/contratos/',function (req, res) {
-    res.render('dashboard',{ title: 'Estandar de Datos de Contrataciones Abiertas', contracts : contracts });
+    edca_db.many('select * from contract').then(function (data) {
+        res.render('dashboard',{ title: 'Estandar de Datos de Contrataciones Abiertas', contracts : data });
+        }).catch(function (error) {
+        console.log("ERROR: ", error);
+    });
 });
 
 /* dashboard contract list by page */
