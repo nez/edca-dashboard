0dd3387f746063d7616950ed30eff0095db6e5e4
Mario Torres <mario291187@gmail.com>
2016-11-02 21:11:46 -0600

    +redcompartida&puertosabiertos

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 //var contrataciones = require('./routes/contrataciones');
+var puertos = require('./routes/puertos');
+var redcompartida = require('./routes/redcompartida');
 
 var app = express();
 
@@ -21,15 +23,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/contratacionesabiertas/static',express.static(path.join(__dirname, 'public')));
+app.use('/puertosabiertos/static',express.static(path.join(__dirname, 'public_puertos')));
+app.use('/redcompartida/static',express.static(path.join(__dirname, 'public_redcompartida')));
 
 //app.use('/', routes);
 app.use('/contratacionesabiertas', routes);
+app.use('/puertosabiertos', puertos);
+app.use('/redcompartida', redcompartida);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,23 +43,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
