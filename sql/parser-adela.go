dcec1361f8cca3e155ef6de83d3cef3b6b6f8538
Andres Gomez Urquiza <andres@fractal.ai>
2017-01-23 14:53:41 -0600

    script de camara original para generar csv de adela

---
+++ sql/parser-adela.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+  "bufio"
+  "encoding/csv"
+  "os"
+  "io"
+  "strings"
+)
+
+func main() {
+  // Load a TXT file.
+  f, _ := os.Open("/Users/beto/Downloads/json_filenames2.csv")
+  r := csv.NewReader(bufio.NewReader(f))
+  w := csv.NewWriter(os.Stdout)
+
+
+  keywords := "contrataciones-abiertas, ocds"
+//  modified := ""
+  contact := "Jaime Madinaveitia Villanueva"
+  mbox := "jaime.madinaveitia@gacm.mx"
+  w.Write([]string {"ds:identifier","ds:title","ds:description","ds:keyword","ds:modified","ds:contactPoint","ds:mbox","ds:temporal","ds:spatial","ds:landingPage","ds:accrualPeriodicity","rs:title","rs:description","rs:downloadURL","rs:mediaType","rs:byteSize","rs:temporal","rs:spatial"})
+
+  for {
+    record, err := r.Read()
+
+    if err == io.EOF {
+      break
+    }
+
+    title := strings.Replace(record[3],"\"","",-1)
+    title = strings.Replace(title,".","",-1)
+
+    id := strings.Replace(record[1],"/","-",-1)
+    id = strings.Replace(id,"_","-",-1)
+    id = strings.Replace(id,".","-",-1)
+/*
+    if strings.Count(title,".") > 1 {
+      fmt.Println(title)
+    }
+*/
+    row := []string {"","\""+title+"\"",record[3],keywords,"18-jul-2016",contact,mbox,"","","","",
+                     "\""+title+" en JSON\"",record[3],"http://cdn.datos.gob.mx/public/gacm/"+id+"/"+record[2],"json","","",""}
+    w.Write(row)
+
+    row = []string {"","\""+title+"\"",record[3],keywords,"18-jul-2016",contact,mbox,"","","","",
+                     "\""+title+" en CSV\"",record[3],"http://cdn.datos.gob.mx/public/gacm/"+id+"/"+id+".csv","csv","","",""}
+     w.Write(row)
+
+    row = []string {"","\""+title+"\"",record[3],keywords,"18-jul-2016",contact,mbox,"","","","",
+                     "\""+title+" en XLSX\"",record[3],"http://cdn.datos.gob.mx/public/gacm/"+id+"/"+id+".xlsx","xlsx","","",""}
+    w.Write(row)
+
+    row = []string {"","\""+title+"\"",record[3],keywords,"18-jul-2016",contact,mbox,"","","","",
+                     "\""+title+" en PDF\"",record[3],"http://cdn.datos.gob.mx/public/gacm/"+id+"/"+id+".pdf","pdf","","",""}
+     w.Write(row)
+
+/*
+    if err := w.Write(row); err != nil {
+      fmt.Println("error writing record to csv:", err)
+    }
+*/
+    //fmt.Println(record[3])
+  }
+  w.Flush()
+
+}
