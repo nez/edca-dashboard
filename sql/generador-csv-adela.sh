eabb65ab15e6db71457047096720633dbc111110
Andres Gomez Urquiza <andres@fractal.ai>
2017-01-24 22:03:15 -0600

    mejoras parser adela

--- sql/generador-csv-adela.sh
+++ sql/generador-csv-adela.sh
@@ -3,4 +3,6 @@
 psql -U tester edca -c "
 copy (select contractingprocess.id as id_local, contractingprocess.ocid, concat(replace(contractingprocess.ocid, '/', '_'),
 '_',contractingprocess.id,'.json') as nombre_archivo, contract.title from contractingprocess, contract where contractingprocess_id = contractingprocess.id  order
-by id_local) to stdout With CSV DELIMITER ','; " >> json_filenames.csv
+by id_local) to stdout With CSV DELIMITER ','; " > json_filenames.csv
+
+go run parser-adela.go > adela-edca.csv
